fd707f1f7441d096d78eeea389414cbf0f1b77b1
Iakkoise <63969450+AndreIakko@users.noreply.github.com>
2022-02-03 16:58:34 +0100

    Add files via upload

---
+++ iak_DrugFarm/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+
+game "gta5"
+
+author 'Iakkoise'
+
+description "iak_DrugFarm"
+
+discord "https://discord.gg/tqk3kAEr4f"
+
+this_is_a_map 'yes'
+
+--cord: -786.5088, -1349.0610, 10.8560
